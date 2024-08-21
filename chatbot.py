a9e6a2f5ebad7a5bd1032a21d0c6740b062ed31a
yunozon <guru.fu.kamie21@gmail.com>
2024-08-21 14:17:11 +0900

    update

--- chatbot.py
+++ chatbot.py
@@ -2,15 +2,36 @@ import streamlit as st
 import google.generativeai as genai
 import os
 
-st.title('Streamlit Chatbot') # タイトルを表示
-
+st.title('Gemini Chatbot') # タイトルを表示
 
 genai.configure(api_key=os.environ["GOOGLE_API_KEY"]) # APIキーを設定
+# TODO: モデルを選択変更予定
+model = genai.GenerativeModel('gemini-1.5-flash') # モデルを選択 (gemini-1.5-flash)
 
-model = genai.GenerativeModel('gemini-1.5-flash') # モデルを選択
+# チャット履歴の初期化
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-prompt = "geminiについて教えて"
-response = model.generate_content(prompt) # テキストを生成
-print(response.text) # 生成されたテキストを表示
+# メッセージを表示
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
 
-# st.write('ここにチャットボットのコードを書いてください')
+# チャット入力欄を表示
+if prompt := st.chat_input("Type something..."):
+    # ユーザが入力したテキストを履歴に追加
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # ユーザーが入力したテキストを表示
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # 応答を生成
+    response = model.generate_content(prompt)
+
+    # 応答を表示
+    with st.chat_message("assistant"):
+        # st.write_stream(response.text)
+        st.markdown(response.text)
+    
+    # 応答を履歴に追加
+    st.session_state.messages.append({"role": "assistant", "content": response.text})
